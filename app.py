b62d6d9db878bf82d9f90407cf231e31814abda8
Gabriel <gabriel.troni13@gmail.com>
2026-06-16 18:45:52 -0300

    feat: renderização da página de login

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Gestor Acadêmico Web (Em construção)</h1>"
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
