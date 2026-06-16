8faae05f359e2d966bfe1feac67983c479828a95
Gabriel <gabriel.troni13@gmail.com>
2026-06-16 14:47:48 -0300

    fix: porta já estava sendo usada

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def index():
     return "<h1>Gestor Acadêmico Web (Em construção)</h1>"
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
